3917b8b2d7a4649248efc9bd8af5449e08f07bde
freddo256 <ferre.99@hotmail.com>
2022-07-07 22:53:18 +0200

    Switched to Angular with JSON CV database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { faEnvelope, faPhone, faGlobe, faUser, faBriefcase, faArchive, faRocket, faHeart, faBookOpen, faBook } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const cv = require('./cv.json');
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'CV-Ansible';
+  title = 'CV Ferre Vekemans';
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faGlobe = faGlobe;
+  faUser = faUser;
+  faBriefcase = faBriefcase;
+  faArchive = faArchive;
+  faRocket = faRocket;
+  faHeart = faHeart;
+  faLinkedinIn = faLinkedinIn; 
+  faGithub = faGithub;
+  faBookOpen = faBookOpen;
+  faBook = faBook;
+  cv = cv;
+  filter = "All";
+  getFilter(): string {
+    return this.filter;
+  }
+  setFilter(value: string): void {
+    this.filter = value;
+  }
+  goToLink(url: string){
+    window.open(url, "_blank");
+  }
 }
